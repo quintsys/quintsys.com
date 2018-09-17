9cf87f91657faaca742c889f91cf427c56db0409
Erich Quintero <qbantek@gmail.com>
2018-09-17 11:17:39 -0400

    add activate event for service worker

--- jekyll/assets/js/sw.js
+++ jekyll/assets/js/sw.js
@@ -11,3 +11,31 @@ self.addEventListener('install', function (event) {
       })
   );
 });
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
