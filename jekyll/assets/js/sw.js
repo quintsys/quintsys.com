cb92a2bfb732a21f8e92f5b3cb700ba3fa1d0a3e
Erich Quintero <qbantek@gmail.com>
2018-09-17 15:01:14 -0400

    Fix?

--- jekyll/assets/js/sw.js
+++ jekyll/assets/js/sw.js
@@ -1,65 +1,25 @@
-var CACHE_NAME = 'qsys-cache-v1';
-var urlsToCache = ['/',
-  '/index.html',
-  '/android-chrome-192x192.png',
-  '/android-chrome-256x256.png',
-  '/apple-touch-icon.png',
-  '/browserconfig.xml',
-  '/favicon-16x16.png',
-  '/favicon-32x32.png',
-  '/favicon.ico',
-  '/mstile-150x150.png',
-  '/safari-pinned-tab.svg'];
+const version = "0.0.1";
+const cacheName = `qsys-${version}`;
+self.addEventListener('install', e => {
+    const timeStamp = Date.now();
+    e.waitUntil(caches.open(cacheName).then(cache => {
+        return cache.addAll(['/', '/index.html']).then(() => self.skipWaiting());
+      }
+    ));
+  }
+);
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+  }
+);
 
-self.addEventListener('fetch', function (event) {
-  console.log('Service Worker Fetch...');
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-
-        if (response) {
-          console.log('Serve from cache', response);
-          return response;
-        }
-
-        var fetchRequest = event.request.clone();
-        return fetch(fetchRequest).then(function (response) {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          var responseToCache = response.clone();
-          caches.open(CACHE_NAME)
-            .then(function (cache) {
-              cache.put(event.request, responseToCache);
-            });
-          return response;
-        });
-      })
-  );
-});
-
-self.addEventListener('activate', function (event) {
-  var cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.open(cacheName).then(cache => cache.match(event.request, {
+      ignoreSearch: true
+    })).then(response => {
+        return response || fetch(event.request);
+      }
+    ));
+  }
+);
