e7ba08a59d1a30b7bf060744f4d48fc0263f8463
Erich Quintero <qbantek@gmail.com>
2018-09-17 12:15:05 -0400

    add to cache

--- jekyll/assets/js/sw.js
+++ jekyll/assets/js/sw.js
@@ -11,16 +11,30 @@ self.addEventListener('install', function (event) {
   );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function (response) {
-          if (response) {
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
             return response;
           }
-          return fetch(event.request);
-        }
-      )
+        );
+      })
   );
 });
 
